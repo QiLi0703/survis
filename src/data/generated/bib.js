743f21eac6eb744aaea6361adf47248293310733
QiLi0703 <liqi970703@gmail.com>
2025-05-03 15:23:15 +0100

    Add 10 papers to bib, run update script, configure properties, add redirect

--- src/data/generated/bib.js
+++ src/data/generated/bib.js
@@ -1,17 +1,107 @@
 ﻿const generatedBibEntries = {
-    "Beck2016Visual": {
-        "abstract": "Bibiographic data such as collections of scientific articles and citation networks have been studied extensively in information visualization and visual analytics research. Powerful systems have been built to support various types of bibliographic analysis, but they require some training and cannot be used to disseminate the insights gained. In contrast, we focused on developing a more accessible visual analytics system, called SurVis, that is ready to disseminate a carefully surveyed literature collection. The authors of a survey may use our Web-based system to structure and analyze their literature database. Later, readers of the survey can obtain an overview, quickly retrieve specific publications, and reproduce or extend the original bibliographic analysis. Our system employs a set of selectors that enable users to filter and browse the literature collection as well as to control interactive visualizations. The versatile selector concept includes selectors for textual search, filtering by keywords and meta-information, selection and clustering of similar publications, and following citation links. Agreement to the selector is represented by word-sized sparkline visualizations seamlessly integrated into the user interface. Based on an analysis of the analytical reasoning process, we derived requirements for the system. We developed the system in a formative way involving other researchers writing literature surveys. A questionnaire study with 14 visual analytics experts confirms that SurVis meets the initially formulated requirements.",
-        "author": "Beck, Fabian and Koch, Sebastian and Weiskopf, Daniel",
-        "doi": "10.1109/TVCG.2015.2467757",
-        "journal": "IEEE Transactions on Visualization and Computer Graphics",
-        "keywords": "type:system, visual_analytics, sparklines, information_retrieval, clustering, literature_browser",
-        "number": "01",
-        "publisher": "IEEE",
-        "series": "TVCG",
-        "title": "Visual Analysis and Dissemination of Scientific Literature Collections with {SurVis}",
+    "arxiv2502_07538": {
+        "archiveprefix": "arXiv",
+        "author": "Liu, Xiaojing and Gurelli, Ogulcan and Wang, Yan and Reiss, Joshua",
+        "doi": "10.48550/arXiv.2502.07538",
+        "eprint": "2502.07538",
+        "primaryclass": "cs.MM",
+        "title": "Visual-based spatial audio generation system for multi-speaker environments",
+        "type": "misc",
+        "url": "https://arxiv.org/abs/2502.07538",
+        "year": "2025"
+    },
+    "arxiv2502_12489": {
+        "archiveprefix": "arXiv",
+        "author": "Ji, Shulei and Wu, Songruoyao and Wang, Zihao and Li, Shuyu and Zhang, Kejun",
+        "doi": "10.48550/arXiv.2502.12489",
+        "eprint": "2502.12489",
+        "primaryclass": "eess.AS",
+        "title": "A Comprehensive Survey on Generative {AI} for Video-to-Music Generation",
+        "type": "misc",
+        "url": "https://arxiv.org/abs/2502.12489",
+        "year": "2025"
+    },
+    "arxiv2503_21254": {
+        "archiveprefix": "arXiv",
+        "author": "Wang, Zhaokai and Bao, Chenxi and Zhuo, Le and Han, Jingrui and Yue, Yang and Tang, Yihong and Huang, Victor Shea-Jay and Liao, Yue",
+        "doi": "10.48550/arXiv.2503.21254",
+        "eprint": "2503.21254",
+        "primaryclass": "cs.CV",
+        "title": "Vision-to-Music Generation: A Survey",
+        "type": "misc",
+        "url": "https://arxiv.org/abs/2503.21254",
+        "year": "2025"
+    },
+    "blanchard_mit_2024": {
+        "author": "Blanchard, Lancelot and Naseck, Perry and Egozy, Eran and Paradiso, Joseph A.",
+        "doi": "https://doi.org/10.21428/e4baedd9.69c11de7",
+        "journal": "An MIT Exploration of Generative AI (PubPub)",
+        "month": "September",
+        "title": "Developing Symbiotic Virtuosity: {AI}-Augmented Musical Instruments and Their Use in Live Music Performances",
         "type": "article",
-        "url": "http://www.visus.uni-stuttgart.de/uploads/tx_vispublications/vast15-survis.pdf",
-        "volume": "22",
-        "year": "2016"
+        "url": "https://doi.org/10.21428/e4baedd9.69c11de7",
+        "year": "2024"
+    },
+    "frontiersVR_2025": {
+        "author": "Erdmann, Matthias and von Berg, Markus and Steffens, Jochen",
+        "doi": "10.3389/frvir.2025.1552321",
+        "journal": "Frontiers in Virtual Reality",
+        "pages": "1552321",
+        "title": "Development and evaluation of a mixed reality music visualization for a live performance based on music information retrieval",
+        "type": "article",
+        "url": "https://doi.org/10.3389/frvir.2025.1552321",
+        "volume": "6",
+        "year": "2025"
+    },
+    "graf_aes_2021": {
+        "author": "Graf, Max and Opara, Harold Chijioke and Barthet, Mathieu",
+        "booktitle": "Audio Engineering Society Convention Paper 10498, AES Show Spring 2021",
+        "doi": "https://doi.org/10.48550/arXiv.2106.10134",
+        "month": "May",
+        "title": "An Audio-Driven System For Real-Time Music Visualisation",
+        "type": "inproceedings",
+        "url": "https://www.aes.org/e-lib/browse.cfm?elib",
+        "year": "2021"
+    },
+    "mozualization_chi_2025": {
+        "author": "Li, Jianing and Liu, Tianyu and Chen, Jingxuan and Ding, Xiaohan and Ma, Xinyu and Liu, Yunhao and Sun, Zhida and Tan, Xu and Wang, Yuancheng and Chen, Haipeng",
+        "booktitle": "Extended Abstracts of the CHI Conference on Human Factors in Computing Systems",
+        "doi": "10.1145/3706599.3719686",
+        "publisher": "ACM",
+        "title": "Mozualization: Crafting Music and Visual Representation with Multimodal {AI}",
+        "type": "inproceedings",
+        "url": "https://doi.org/10.1145/3706599.3719686",
+        "year": "2025"
+    },
+    "procedural_audio_jaes_2023": {
+        "author": "Menzies, Dylan and Ronan, David and Reiss, Joshua D.",
+        "doi": "https://doi.org/10.17743/jaes.2022.0108",
+        "journal": "Journal of the Audio Engineering Society",
+        "month": "December",
+        "number": "12",
+        "pages": "826--847",
+        "title": "The State of the Art in Procedural Audio",
+        "type": "article",
+        "url": "http://www.eecs.qmul.ac.uk/~josh/documents/2023/Procedural%20audio%20review.pdf",
+        "volume": "71",
+        "year": "2023"
+    },
+    "robertson_rt_music_gen_ve_1998": {
+        "author": "Robertson, J. and Quincey, A. D. and Stapleford, Tom and Wiggins, Geraint A.",
+        "booktitle": "Proceedings of ECAI-98 Workshop on AI/Alife and Entertainment",
+        "doi": "",
+        "title": "Real-Time Music Generation for a Virtual Environment",
+        "type": "inproceedings",
+        "url": "https://www.semanticscholar.org/paper/Real-time-music-generation-for-a-virtual-Robertson-Stapleford/97f1d5d26c784e915d249f638c025f0c1f17b0e9",
+        "year": "1998"
+    },
+    "taylor2006real": {
+        "author": "Taylor, Robyn and Boulanger, Pierre and Torres, Daniel",
+        "booktitle": "8th International Conference on Virtual Reality",
+        "organization": "Citeseer",
+        "pages": "26--30",
+        "title": "Real-time music visualization using responsive imagery",
+        "type": "inproceedings",
+        "year": "2006"
     }
 };
